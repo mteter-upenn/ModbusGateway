4e0e3d54c36104ba6671c3f08d1f939ede7a2b7c
Matt Teter <mteter@upenn.edu>
2016-10-06 11:01:35 -0400

    10/6/16, add new library, ModbusStack

---
+++ mc_programs/libraries/ModbusStack/ModbusStack.h
@@ -0,0 +1,16 @@
+
+#ifndef ModbusStack_h
+#define ModbusStack_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class ModbusStack {
+
+};
+
+#endif
+
