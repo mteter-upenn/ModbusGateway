e6b3dc67ab2ba4ae01fa09a0dc19b2398f53751d
Matt Teter <mteter@upenn.edu>
2017-01-06 15:24:02 -0500

    1/6/17, json tests

---
+++ mc_programs/jsonTest/jsonTest.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		jsonTest.ino
+ Created:	1/6/2017 2:24:11 PM
+ Author:	mteter
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <ArduinoJson.h>
+
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
