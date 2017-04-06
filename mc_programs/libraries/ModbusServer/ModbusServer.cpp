c24a480944aae89b102ed1c60c1a64a12e22dc40
Matt Teter <mteter@upenn.edu>
2017-04-06 14:55:25 -0400

    4/6/17, correction to ModbusServer begin()

--- mc_programs/libraries/ModbusServer/ModbusServer.cpp
+++ mc_programs/libraries/ModbusServer/ModbusServer.cpp
@@ -14,34 +14,60 @@ ModbusServer::ModbusServer(uint8_t u8_serialPort, uint8_t u8_enablePin) {
 }
 
 
-void ModbusServer::begin() {
-	begin(19200);
-}
-
-
-void ModbusServer::begin(uint16_t u16_baudRate) {
-	if (m_u8_enablePin != 255) {
-		pinMode(m_u8_enablePin, OUTPUT);
-		digitalWrite(m_u8_enablePin, LOW); // LOW is Tx enabled
+void ModbusServer::begin(uint16_t u16_baudRate, uint8_t u8_dataBits, uint8_t u8_parity, uint8_t u8_stopBits) {
+  if (m_u8_enablePin != 255) {
+    pinMode(m_u8_enablePin, OUTPUT);
+    digitalWrite(m_u8_enablePin, LOW); // LOW is Tx enabled
   }
-	
-	switch (m_u8_serialPort) {
+
+  switch (m_u8_serialPort) {
 #if defined(CORE_TEENSY)
-		case 3:
-			m_MBSerial = &Serial3;
-			break;
+    case 3:
+      m_MBSerial = &Serial3;
+      break;
     case 0:
     default:
-			m_MBSerial = &Serial1; ///< Pointer to Serial class object
+      m_MBSerial = &Serial1; ///< Pointer to Serial class object
 #else
-		case 0:
+    case 0:
     default:
       m_MBSerial = &Serial;
 #endif
       break;
-	}
-	
-	m_MBSerial->begin(u16_baudRate);
+  }
+
+  uint32_t u32_serialFormat = SERIAL_8N1;
+  if (u8_dataBits == 8) {
+    if (u8_parity == 0) {
+      if (u8_stopBits == 2) {
+        u32_serialFormat = SERIAL_8N2;
+      }
+    }
+    else if (u8_parity == 1) {
+      if (u8_stopBits == 1) {
+        u32_serialFormat = SERIAL_8O1;
+      }
+    }
+    else if (u8_parity == 2) {
+      if (u8_stopBits == 1) {
+        u32_serialFormat = SERIAL_8E1;
+      }
+    }
+  }
+  else if (u8_dataBits == 7) {
+    if (u8_parity == 1) {
+      if (u8_stopBits == 1) {
+        u32_serialFormat = SERIAL_7O1;
+      }
+    }
+    else if (u8_parity == 2) {
+      if (u8_stopBits == 1) {
+        u32_serialFormat = SERIAL_7E1;
+      }
+    }
+  }
+
+  m_MBSerial->begin(u16_baudRate, u32_serialFormat);
 }
 
 
@@ -686,7 +712,7 @@ uint8_t ModbusServer::parseRequest(EthernetClient52 &ec_client, ModbusRequest &m
 				// SlaveData.getRedTypeByInd(u8_slvInd, mbReq.u8_mtrType);
 				
 				mbReq.u8_id = SlaveData[u8_slvInd].u8_id;
-				mbReq.u8_mtrType = SlaveData[u8_slvInd].u8a_type[0];
+        mbReq.u8_mtrType = SlaveData[u8_slvInd].u8a_mtrType[0];
 					
 				if (SlaveData.isSlaveTcpByInd(u8_slvInd)) {  // if slave is tcp
 					mbReq.u8_flags = MRFLAG_isTcp;
