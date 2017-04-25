e03a117684609915283c504a2e76e0a3339c3f65
Matt Teter <mteter@upenn.edu>
2017-04-25 16:21:28 -0400

    4/25/17, begun making FRAM lib with FIFO

--- mc_programs/libraries/ExternalFRAM/ExternalFRAM.cpp
+++ mc_programs/libraries/ExternalFRAM/ExternalFRAM.cpp
@@ -1,1 +1,50 @@
 
+#include "ExternalFRAM.h"
+
+#if defined(__arm__)
+#include "SPIFIFO.h"
+#ifdef  HAS_SPIFIFO
+#define USE_SPIFIFO
+#endif
+#endif
+
+
+#include "Arduino.h"
+
+
+uint8_t ExtFRAMClass::init() {
+  delay(200);  // unsure if necessary, copied from w5200
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+
+#ifdef USE_SPIFIFO
+  SPI.begin();
+  SPIFIFO.begin(5, SPI_CLOCK_24MHz);  // 5 is pin for SS
+#else
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+  initSS();
+#endif
+
+  SPI.beginTransaction(SPI_EXTFRAM_SETTINGS);
+
+  SPIFIFO.clear();
+  SPIFIFO.write(0x9F);
+
+  uint32_t u32_test;
+
+  for (int ii = 0; ii < 4; ++ii) {
+    u32_test = SPIFIFO.read();
+    SPIFIFO.write(0);
+    Serial.print(ii, DEC); Serial.print(": ");
+    Serial.println(u32_test, DEC);
+  }
+
+  SPI.endTransaction();
+
+
+
+  return 1;
+}
+
--- mc_programs/libraries/ExternalFRAM/ExternalFRAM.h
+++ mc_programs/libraries/ExternalFRAM/ExternalFRAM.h
@@ -3,6 +3,17 @@
 
 #include "Arduino.h"
 
+#include <SPI.h>
 
+#define SPI_EXTFRAM_SETTINGS SPISettings(24000000, MSBFIRST, SPI_MODE0)  // was 14000000
+//#define SPI_EXTFRAM_SETTINGS SPISettings(8000000, MSBFIRST, SPI_MODE0)
+//#define SPI_EXTFRAM_SETTINGS SPISettings(4000000, MSBFIRST, SPI_MODE0)
 
+class ExtFRAMClass {
+public:
+  static uint8_t init();
+
+};
+
+extern ExtFRAMClass ExtFRAM;
 #endif
